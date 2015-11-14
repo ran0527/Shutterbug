b8fd525b13cfbdf361062efcc349915926202bf6
Xinran <lindashi527@gmail.com>
2015-11-13 16:10:31 -0800

    Initial Commit

---
+++ Shutterbug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by Kaiqi on 11/13/15.
+//  Copyright © 2015 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
